6449bd698aef628fd4d601b2acb9fa4fc295bc60
AlferKirill <99204127+AlferKirill@users.noreply.github.com>
2026-08-20 21:48:48 +0300

    Update 3ar.sql

--- HW5/3ar.sql
+++ HW5/3ar.sql
@@ -2,13 +2,13 @@ SELECT
     d.DayName AS DayOfWeek,
     ti.Period,
     ti.Hour,
-    COUNT(f.AttendanceID) AS BookingsCount,
-    COUNT(DISTINCT f.ClientID) AS UniqueClients,
-    ROUND(COUNT(f.AttendanceID)::NUMERIC / COUNT(DISTINCT f.ScheduleID), 2) AS AvgOccupancy
+    COUNT(f.AttendanceID) AS BookingsCount,  -- Количество записей в этот час
+    COUNT(DISTINCT f.ClientID) AS UniqueClients, -- Количество уникальных клиентов
+    ROUND(COUNT(f.AttendanceID)::NUMERIC / COUNT(DISTINCT f.ScheduleID), 2) AS AvgOccupancy -- Средняя заполняемость зала
 FROM FactAttendance f
-JOIN DimDate d ON f.DateID = d.DateID
-JOIN DimTime ti ON f.TimeID = ti.TimeID
-WHERE f.Status != 'отменена'
+JOIN DimDate d ON f.DateID = d.DateID -- Присоединяем календарь для получения дня недели
+JOIN DimTime ti ON f.TimeID = ti.TimeID  -- Присоединяем время для получения часа и периода
+WHERE f.Status != 'отменена' -- Исключаем отменённые записи
 GROUP BY d.DayName, ti.Period, ti.Hour, d.DayOfWeek
-ORDER BY BookingsCount DESC
-LIMIT 10;
+ORDER BY BookingsCount DESC-- Сортируем по популярности (от самого популярного слота)
+LIMIT 10; -- Показываем только топ 10 временных слотов
