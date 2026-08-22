3911e1f735a6dbe13e382c588457a82abc9aab5c
Кирилл Альфер <alferkiruha@gmail.com>
2026-08-22 13:57:15 +0300

    Add homework 6

---
+++ HW6/main.py
@@ -0,0 +1,11 @@
+num1 = float(input('Введите первое число: '))
+num2 = float(input('Введите второе число: '))
+operator = input("Выберите оператор (+,-,*,/): ")
+if operator == '+':
+    print(num1 + num2)
+elif operator == '-':
+    print(num1 - num2)
+elif operator == '*':
+    print(num1 * num2)
+elif operator == '/':
+    print(num1 / num2)
